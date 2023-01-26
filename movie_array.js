11eb7a32428cd4c5e73c1a504429fe90176e9d71
Camille <c.gonzalez0017@gmail.com>
2023-01-25 20:48:27 -0600

    updated array assignment

--- movie_array.js
+++ movie_array.js
@@ -1,2 +1,10 @@
 const myFavoriteMovies = ["Spirited Away", "Everything Everywhere All at Once", "Into the Spider-Verse", "Farewell My Concubine", "Coco"];
 console.log(myFavoriteMovies);
+let movie1 = prompt("what is your favorite movie?")
+myFavoriteMovies.push(movie1);
+console.log("updated favorite movies", myFavoriteMovies);
+
+//display full list of movies with toString() method
+console.log(myFavoriteMovies.toString());
+alert("My favorite movies are....")
+alert(myFavoriteMovies);
